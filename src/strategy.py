756f8a32268508543723d27538c8f15b22c9ba09
neo1 <tobby2002@gmailcom>
2019-04-11 16:38:37 +0900

    add sort=True

--- src/strategy.py
+++ src/strategy.py
@@ -78,7 +78,6 @@ class Rci(Bot):
 
     def strategy(self, open, close, high, low, volume):
         lot = self.exchange.get_lot()
-
         itv_s = self.input('rcv_short_len', int, 5)
         itv_m = self.input('rcv_medium_len', int, 9)
         itv_l = self.input('rcv_long_len', int, 15)
@@ -244,5 +243,122 @@ class Sample(Bot):
         which = random.randrange(2)
         if which == 0:
             self.exchange.entry("Long", True, lot)
+            logger.info(f"Trade:Long")
         else:
             self.exchange.entry("Short", False, lot)
+            logger.info(f"Trade:Short")
+
+
+# Neo Nissi Strategy
+class Cross(Bot):
+    def __init__(self):
+        Bot.__init__(self, '1m')
+
+    def options(self):
+        return {
+            'fast_len': hp.quniform('fast_len', 1, 30, 1),
+            'slow_len': hp.quniform('slow_len', 1, 30, 1),
+        }
+
+    def strategy(self, open, close, high, low, volume):
+        lot = self.exchange.get_lot()
+        fast_len = self.input('fast_len', int, 9)
+        slow_len = self.input('slow_len', int, 16)
+        fast_sma = sma(close, fast_len)
+        slow_sma = sma(close, slow_len)
+        golden_cross = crossover(fast_sma, slow_sma)
+        dead_cross = crossunder(fast_sma, slow_sma)
+
+        logger.info("lot:"+str(lot))
+        # logger.info("data:"+str(self.exchange.data))
+        # logger.info("fast_sma:"+str(fast_sma))
+        # logger.info("slow_sma:"+str(slow_sma))
+
+        if golden_cross:
+            self.exchange.entry("Long", True, lot)
+            logger.info(f"golden_cross trade : Long")
+        if dead_cross:
+            self.exchange.entry("Short", False, lot)
+            logger.info(f"dead_cross trade : Short")
+
+# OCC2
+class OCC2(Bot):
+    variants = [sma, ema, double_ema, triple_ema, wma, ssma, hull]
+    eval_time = None
+
+    def __init__(self):
+        Bot.__init__(self, '1m')
+
+    def ohlcv_len(self):
+        return 15 * 30
+
+    def options(self):
+        return {
+            'variant_type': hp.quniform('variant_type', 0, len(self.variants) - 1, 1),
+            'basis_len': hp.quniform('basis_len', 1, 30, 1),
+            'resolution': hp.quniform('resolution', 1, 10, 1),
+            'sma_len': hp.quniform('sma_len', 1, 15, 1),
+            'div_threshold': hp.quniform('div_threshold', 1, 6, 0.1),
+        }
+
+    def strategy(self, open, close, high, low, volume):
+        lot = self.exchange.get_lot()
+        lot = 1
+
+        variant_type = self.input(defval=5, title="variant_type", type=int)
+        basis_len = self.input(defval=19,  title="basis_len", type=int)
+        resolution = self.input(defval=2, title="resolution", type=int)
+        sma_len = self.input(defval=9, title="sma_len", type=int)
+        div_threshold = self.input(defval=3.0, title="div_threshold", type=float)
+
+        source = self.exchange.security(str(resolution) + 'm')
+
+        if self.eval_time is not None and \
+                self.eval_time == source.iloc[-1].name:
+            return
+
+        series_open = source['open'].values
+        series_close = source['close'].values
+
+        variant = self.variants[variant_type]
+
+        val_open = variant(series_open,  basis_len)
+        val_close = variant(series_close, basis_len)
+
+        if val_open[-1] > val_close[-1]:
+            high_val = val_open[-1]
+            low_val = val_close[-1]
+        else:
+            high_val = val_close[-1]
+            low_val = val_open[-1]
+
+        sma_val = sma(close, sma_len)
+
+        self.exchange.plot('val_open', val_open[-1], 'b')
+        self.exchange.plot('val_close', val_close[-1], 'r')
+
+
+        logger.info("lot:"+str(lot))
+        # logger.info("data:"+str(self.exchange.data))
+        logger.info("sma_val[-1]:"+str(sma_val[-1]))
+        logger.info("low_val:"+str(low_val))
+        logger.info("high_val:"+str(high_val))
+        logger.info("math.floor(low_val):"+str(math.floor(low_val)))
+        logger.info("math.ceil(high_val):"+str(math.ceil(high_val)))
+
+        self.exchange.entry("Long", True,   lot, stop=math.floor(low_val), when=(sma_val[-1] < low_val))
+        if sma_val[-1] < low_val:
+            logger.info(f"OCC2 trade : Long")
+
+        self.exchange.entry("Short", False, lot, stop=math.ceil(high_val), when=(sma_val[-1] > high_val))
+        if sma_val[-1] > high_val:
+            logger.info(f"OCC2 trade : Short")
+
+        open_close_div = sma(numpy.abs(val_open - val_close), sma_len)
+
+        if open_close_div[-1] > div_threshold and \
+                open_close_div[-2] > div_threshold < open_close_div[-2]:
+            logger.info(f"OCC2 trade : close_all")
+            self.exchange.close_all()
+
+        self.eval_time = source.iloc[-1].name
